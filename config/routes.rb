9480953febfd7931842112eb19bf6c334109a268
imanuel lombardo <imanuel.lombardo@hotmail.com>
2021-11-30 13:04:12 +0100

    route challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'challenge/show'
-  get 'challenge/new'
-  get 'challenge/create'
-  get 'missions/index'
-  get 'missions/show'
   devise_for :users
   root to: 'pages#home'
-
+  resources :challenges, only: [:show]
   resources :missions, only: [:index, :show] do
-    resources :challenges, only: [:show, :new, :create]
+    resources :challenges, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

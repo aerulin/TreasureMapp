48d40458a7dbaf0ea059fbaeee529a0bd8d564b8
imanuel lombardo <imanuel.lombardo@hotmail.com>
2021-11-30 13:40:43 +0100

    correct file

--- test/controllers/challenges_controller_test.rb
+++ test/controllers/challenges_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ChallengeControllerTest < ActionDispatch::IntegrationTest
+class ChallengesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get challenge_show_url
     assert_response :success

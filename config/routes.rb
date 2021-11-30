8c47dbd1417d6011eb868a8df0fbefc6680f5dd7
imanuel lombardo <imanuel.lombardo@hotmail.com>
2021-11-30 11:51:30 +0100

    generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'challenge/show'
+  get 'challenge/new'
+  get 'challenge/create'
   get 'missions/index'
   get 'missions/show'
   devise_for :users
   root to: 'pages#home'
-  resources :missions, only: [:index, :show]
+
+  resources :missions, only: [:index, :show] do
+    resources :challenges, only: [:show, :new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

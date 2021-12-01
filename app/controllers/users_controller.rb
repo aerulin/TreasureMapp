f62267cd1c34e673e89fa6187092c991c6d0e69f
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-12-01 16:02:26 +0100

    landing page created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     # Gathers all users sorted by score (through a class method in User model)
     @users = User.all.sort_by { |user| -user.calculate_score }
   end
+
+  def playground
+    @user = current_user
+    @leaderboard = User.all.sort_by { |user| -user.calculate_score }
+    @challenges = Challenge.where(user: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  resources :users, only: [:index]
+  root to: 'users#playground'
+
+  resources :users, only: [:index] do
+    collection do
+      get :playground
+    end
+  end
+
   resources :missions, only: [:index, :show] do
     resources :challenges, only: [:create]
   end
+
   resources :challenges, only: [:show] do
     resources :questions do
       post 'validate', as: :validate, to: 'challenge_questions#validate'
       get 'new', to: 'challenge_questions#new'
     end
   end
+
 end

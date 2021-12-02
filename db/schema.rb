f0439588331465cfbd0251f907ddadc2528fa87a
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-12-02 11:59:17 +0100

    updated DB + seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_090454) do
+ActiveRecord::Schema.define(version: 2021_12_02_102004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_090454) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.string "city"
   end
 
   create_table "questions", force: :cascade do |t|

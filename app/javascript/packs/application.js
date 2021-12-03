314928765d598c633a67382664aeefe99145b26e
Julie Queloz <juliequeloz@hotmail.com>
2021-12-03 11:37:19 +0100

    bugfix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -30,8 +30,8 @@ import { initMapbox } from '../plugins/init_mapbox';
 // import { initSelect2 } from '../components/init_select2';
 
 
-// document.addEventListener('turbolinks:load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   initMapbox();
-// });
+});
 
 // CSS

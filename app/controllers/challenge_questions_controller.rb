9eb5529b7d89eca0e4b42600457f384dcbbe028e
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-12-07 11:13:15 +0100

    new validation working

--- app/controllers/challenge_questions_controller.rb
+++ app/controllers/challenge_questions_controller.rb
@@ -12,17 +12,21 @@ class ChallengeQuestionsController < ApplicationController
 
     challenge_question = ChallengeQuestion.find_or_create_by(question: question, challenge: challenge)
     challenge_question.answer_counter += 1
+    user_guess = params[:challenge_question][:guess].downcase.split
+    question_validation = question.validation
+    intersection = user_guess & question_validation
 
-    if params[:challenge_question][:guess].downcase == question.answer.downcase
+    if intersection.empty?
+      challenge_question.save!
+      flash[:false] = "Fausse réponse! Essayez à nouveau."
+    else
       challenge_question.status = true
       challenge_question.save!
       flash[:true] = "Bravo! Vous avez trouvé la bonne réponse."
-    else
-      challenge_question.save!
-      flash[:false] = "Fausse réponse! Essayez à nouveau."
     end
     redirect_to challenge_map_path(challenge)
   end
+
 
   private
 
